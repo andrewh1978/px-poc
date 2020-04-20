e1284bb2bfed11fd215169e709b0f4ca3bf16c5b
Andrew Hill <andrew@portworx.com>
2020-04-20 16:41:34 +0100

    add tmux windows

---
+++ go.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+trap '' 2
+export KUBECONFIG=/kubeconfig
+export LANG=en_US.UTF-8
+tmux new-session -d
+tmux split-window -d -l 30 "k9s --kubeconfig /kubeconfig -A"
+tmux split-window -d -l 20 "kubectl logs -f -lname=portworx -n kube-system"
+tmux split-window -d -t 1 -h top
+tmux split-window -d -p 100 /menu.sh
+tmux kill-pane -t 0
+tmux attach
