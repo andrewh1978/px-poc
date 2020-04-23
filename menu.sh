1377906dd4c43e5bd8bf76c258088d7499d74083
Andrew Hill <andrew@portworx.com>
2020-04-23 11:06:51 +0000

    change panes, one for shell output and the other for an empty bash

--- menu.sh
+++ menu.sh
@@ -9,18 +9,21 @@ DIALOG_CANCEL=1
 DIALOG_ESC=255
 HEIGHT=0
 WIDTH=0
-
-display_result() {
-  result=$(eval $2)
-  dialog --title "$1" \
-    --no-collapse \
-    --msgbox "$result" $HEIGHT $WIDTH
-}
+NC='\033[0m'
+BOLD='\033[1m'
+LOG=/log
+echo -ne "$BOLD# ">$LOG
 
 display_info() {
   dialog --title "$1" \
     --no-collapse \
     --msgbox "$2" $HEIGHT $WIDTH
+}
+
+run_master() {
+  echo -e "$1$NC" >>$LOG
+  eval $1 >>$LOG 2>&1
+  echo -ne "$BOLD# ">>$LOG
 }
 
 menu_top() {
@@ -96,52 +99,58 @@ menu_failover() {
   done
 }
 
-test_failover_postgres() {
-  tmux split-window -d -l 20 "kubectl get nodes -w"
-  tmux split-window -d -t 1 -h "kubectl get pods -w -o wide"
-  display_info "Deploy PostgreSQL" "First, we will deploy PostgreSQL"
-  display_result "Deploy PostgreSQL" "kubectl apply -f /assets/deploy_postgres.yml"
-  kubectl wait pod -lapp=postgres --for=condition=ready --timeout=120s
-  display_info "Get pods" "Now, let's look at the pods to see where PostgreSQL was provisioned"
-  display_result "Get pods" "kubectl get pods -lapp=postgres -o wide"
-  node=$(kubectl get pods -lapp=postgres -A -o=custom-columns='DATA:spec.nodeName' --no-headers)
-  display_info "Cordon node" "Next, we will cordon the node"
-  display_result "Cordon node" "kubectl cordon $node"
-  display_info "Delete pod" "Now, we can delete the pod and watch it scheduled on another node"
-  display_result "Delete pod" "kubectl delete pod -lapp=postgres"
-  kubectl wait pod -lapp=postgres --for=condition=ready --timeout=120s
-  display_info "Get pods" "Look at the pods again to see where it has been scheduled"
-  display_result "Get pods" "kubectl get pods -lapp=postgres -o wide"
-  display_info "Uncordon node" "Uncordon the node"
-  display_result "Uncordon node" "kubectl uncordon $node"
-  display_info "Delete PostgreSQL" "Finally, delete the PostgreSQL deployment"
-  display_result "Delete PostgreSQL" "kubectl delete -f /assets/deploy_postgres.yml"
+create_demo_panes() {
+  tmux split-window -d -h "tail -s 0.1 -f $LOG"
+  tmux split-window -d
+}
+
+destroy_demo_panes() {
   tmux kill-pane -t 1
   tmux kill-pane -t 1
 }
 
+test_failover_postgres() {
+  create_demo_panes
+  display_info "Deploy PostgreSQL" "First, we will deploy PostgreSQL"
+  run_master "kubectl apply -f /assets/deploy_postgres.yml"
+  run_master "kubectl wait pod -lapp=postgres --for=condition=ready --timeout=120s"
+  display_info "Get pods" "Now, let's look at the pods to see where PostgreSQL was provisioned"
+  run_master "kubectl get pods -lapp=postgres -o wide"
+  node=$(kubectl get pods -lapp=postgres -A -o=custom-columns='DATA:spec.nodeName' --no-headers)
+  display_info "Cordon node" "Next, we will cordon the node"
+  run_master "kubectl cordon $node"
+  display_info "Delete pod" "Now, we can delete the pod and watch it scheduled on another node"
+  run_master "kubectl delete pod -lapp=postgres"
+  run_master "kubectl wait pod -lapp=postgres --for=condition=ready --timeout=120s"
+  display_info "Get pods" "Look at the pods again to see where it has been scheduled"
+  run_master "kubectl get pods -lapp=postgres -o wide"
+  display_info "Uncordon node" "Uncordon the node"
+  run_master "kubectl uncordon $node"
+  display_info "Delete PostgreSQL" "Finally, delete the PostgreSQL deployment"
+  run_master "kubectl delete -f /assets/deploy_postgres.yml"
+  destroy_demo_panes
+}
+
 test_failover_mysql() {
-  tmux split-window -d -l 20 "kubectl get nodes -w"
-  tmux split-window -d -t 1 -h "kubectl get pods -w -o wide"
+  create_demo_panes
   display_info "Deploy MySQL" "First, we will deploy MySQL"
-  display_result "Deploy MySQL" "kubectl apply -f /assets/deploy_mysql.yml"
-  kubectl wait pod -lapp=mysql --for=condition=ready --timeout=120s
+  run_master "kubectl apply -f /assets/deploy_mysql.yml"
+  run_master "kubectl wait pod -lapp=mysql --for=condition=ready --timeout=120s"
   display_info "Get pods" "Now, let's look at the pods to see where MySQL was provisioned"
-  display_result "Get pods" "kubectl get pods -lapp=mysql -o wide"
+  run_master "kubectl get pods -lapp=mysql -o wide"
   node=$(kubectl get pods -lapp=mysql -A -o=custom-columns='DATA:spec.nodeName' --no-headers)
   display_info "Cordon node" "Next, we will cordon the node"
-  display_result "Cordon node" "kubectl cordon $node"
+  run_master "kubectl cordon $node"
   display_info "Delete pod" "Now, we can delete the pod and watch it scheduled on another node"
-  display_result "Delete pod" "kubectl delete pod -lapp=mysql"
-  kubectl wait pod -lapp=mysql --for=condition=ready --timeout=120s
+  run_master "kubectl delete pod -lapp=mysql"
+  run_master "kubectl wait pod -lapp=mysql --for=condition=ready --timeout=120s"
   display_info "Get pods" "Look at the pods again to see where it has been scheduled"
-  display_result "Get pods" "kubectl get pods -lapp=mysql -o wide"
+  run_master "kubectl get pods -lapp=mysql -o wide"
   display_info "Uncordon node" "Uncordon the node"
-  display_result "Uncordon node" "kubectl uncordon $node"
+  run_master "kubectl uncordon $node"
   display_info "Delete MySQL" "Finally, delete the MySQL deployment"
-  display_result "Delete MySQL" "kubectl delete -f /assets/deploy_mysql.yml"
-  tmux kill-pane -t 1
-  tmux kill-pane -t 1
+  run_master "kubectl delete -f /assets/deploy_mysql.yml"
+  destroy_demo_panes
 }
 
 menu_top
